b0550c488023ff62760d6b02124354339eb529eb
Guillaume Barbey <thekeproox@gmail.com>
2018-01-15 18:38:03 +0100

    Fichiers

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,248 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+//Needed for using an OpenFileDialog and a streamReader
+
+using System.IO;
+using Microsoft.Win32;
+
+
+
+namespace caesarEncrypt
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        char[] cTextAfter;
+
+        //Variables
+        int iKey = 0; // value of the key
+        string text; // contains the text given by the user
+        char[] cText; // contains the text in ASCII code
+        int idx = 0; //loop index
+        int iValue; // value in int of the ASCII caracter
+
+        int iNewLetter; //The new ASCII caracter
+        bool bCorrect = false; //test if operation done correctly
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Button encrypting the given text
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public void btnEncrypt_Click(object sender, RoutedEventArgs e)
+        {
+            //Algorythm
+
+            txtResult.Clear();
+            text = txtText.Text;
+            cText = new char[text.Length];
+            cTextAfter = new char[text.Length];
+
+            text = text.ToLower();
+            try
+            {
+                iKey = int.Parse(txtKey.Text);
+            }
+            catch (Exception)
+            {
+
+                txtKey.Text = "Put a correct key";
+
+            }
+            //Test on the key
+
+            if (iKey >= 1 && iKey <= 26)
+            {
+                //all the process
+                bCorrect = true;
+                ControlLetter();  
+            }
+            else
+            {
+                txtKey.Text = "Put a correct Key";
+            }
+
+            //display mothafucka
+            //if the boolean is true, write the result (counter some bug)
+            if (bCorrect)
+            {
+                ShowText();
+            }
+            
+            
+        }
+
+        private void btnSave_Click(object sender, RoutedEventArgs e)
+        {
+            string ligne;                       // pour la lecture de la ligne en cours du texte source
+            int iLigne;                         // Index de boucle
+            int nbreLignes;
+
+            OpenFileDialog ofd; //déclaration du choix de sélection
+            ofd = new OpenFileDialog(); //instanciation de celui ci                          
+            ofd.InitialDirectory = Directory.GetCurrentDirectory(); //On indique dans quel répertoire il doit démarrer                       
+            ofd.ShowDialog(); //On l'affiche ensuite à l'écran
+
+            if (ofd.FileName != "")
+            {
+                FileInfo fFichierSrce = new FileInfo(ofd.FileName); //On déclare un fichier FileInfo qui stockera les informations du répertoire    
+                StreamWriter strmr = new StreamWriter(ofd.FileName, false, Encoding.Default); //On ouvre un flux de lecture dans le répertoire de la boite de sélection
+
+                //On lis les lignes et on les stockes dans les variables
+
+                nbreLignes = txtResult.LineCount;
+                for (iLigne = 0; iLigne < nbreLignes; iLigne++)
+                {
+                    ligne = txtResult.GetLineText(iLigne);
+                    strmr.Write(ligne);
+                }
+
+                //fermeture du flux
+                strmr.Close();
+
+                //On lance notepad++
+                try
+                {
+
+                    System.Diagnostics.Process.Start("NotePad++.exe", ofd.FileName);
+
+                }
+                catch (Exception)
+                {
+                    lblAlert.Content = "Impossible d'ouvrir NotePad++";
+                }
+            }
+            else
+            {
+                lblAlert.Content = "Veuillez choisir un fichier";
+            }
+        }
+
+        private void btnShowWin_Click(object sender, RoutedEventArgs e)
+        {
+            WinDecrypt win = new WinDecrypt();
+            win.Show();
+        }
+
+        public void ShowText()
+        {
+            int iKeyCrypted = 96 + iKey;
+            char cKeyCrypted = (char)iKeyCrypted;
+            txtResult.Text += " " +cKeyCrypted;
+
+        }
+     
+        public void ControlLetter()
+        {
+            foreach (char c in text)
+            {
+
+                cText[idx] = c;
+                iValue = cText[idx];
+
+                //if value is an ' word caracter
+                if (iValue == 8217)
+                {
+                    iValue = 39;
+                }
+
+                iNewLetter = iValue + iKey;
+
+                //if the new letter is superior to z and different thant an space and 
+                if (iNewLetter > 122 && iNewLetter != 32 && iValue <= 122)
+                {
+                    int iReste = iNewLetter - 122;
+                    int iNouvValeur = 96 + iReste;
+                    cTextAfter[idx] = (char)iNouvValeur;
+                }
+                if (iNewLetter <= 122 && iNewLetter >= 96)
+                {
+                    cTextAfter[idx] = (char)iNewLetter;
+                }
+                else
+                {
+                    if (iValue >= 0 && iValue <= 64 || iValue > 200)
+                    {
+                        cTextAfter[idx] = (char)iValue;
+                    }
+                    else
+                    {
+                        if (iValue == 233 || iValue == 232 || iValue == 234)
+                        {
+                            int ValeurCorrecte = 101 + iKey;
+                            cTextAfter[idx] = (char)ValeurCorrecte;
+                        }
+                        if (iValue == 224)
+                        {
+                            int ValeurCorrecte = 97 + iKey;
+                            cTextAfter[idx] = (char)ValeurCorrecte;
+                        }
+                    }
+                    if (iValue == 123 || iValue == 125 || iValue == 91 || iValue == 93)
+                    {
+                        cTextAfter[idx] = (char)iValue;
+                    }
+                }
+                txtResult.Text += cTextAfter[idx].ToString();
+                idx++;
+            }
+        }
+
+        private void btnEncryptVig_Click(object sender, RoutedEventArgs e)
+        {
+            //Variables
+            int idxList = 0;
+            char c;
+            int LetterA = 96;
+            int iEcart = 0;
+            int iEcartTotal = 0;
+            List<char> charList;
+            charList = new List<char>();
+            List<bool> bPositionLetter = new List<bool>(); //Permettra de savoir l'emplacement du mot dans le tableau
+
+            while (idxList < 26*26)
+            {
+                LetterA++;
+                iEcartTotal = LetterA + iEcart;
+                
+                c = (char)iEcartTotal;
+
+                if (c <= 122 && c > 96)
+                {
+                    charList.Add(c);
+                }
+                else
+                {
+                    iEcart++;
+                    LetterA = 96;
+                    iEcartTotal = LetterA + iEcart;
+                    charList.Add(c);
+                }
+                idxList++;
+            }
+            txtResult.Text = charList[idxList].ToString();
+
+            
+        }
+    }
+}
---
+++ WinDecrypt.xaml.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.IO;
+using Microsoft.Win32;
+
+namespace caesarEncrypt
+{
+    /// <summary>
+    /// Logique d'interaction pour WinDecrypt.xaml
+    /// </summary>
+    public partial class WinDecrypt : Window
+    {
+
+
+        //Variables
+        int iKey = 0;
+        string text;
+        char[] cText;
+        int idx = 0;
+        int iValue;
+        int iGap;
+        int iNewLetter;
+        bool bCorrect = false;
+
+
+        string sLigne;                      // variable utilisée pour stocker les lignes du fichier texte
+        string sLignes;                     // variable stockant le total du fichier texe   
+        int iNbrLignes = 0;                 // variable stockant le nombres de lignes du fichier
+
+
+
+
+        public WinDecrypt()
+        {
+            InitializeComponent();
+        }
+
+        private void btnDecrypt_Click(object sender, RoutedEventArgs e)
+        {
+
+            
+            
+
+            //Algorythm
+            char[] cTextAfter;
+
+            txtDecrypted.Clear();
+
+            text = txtDecrypt.Text;
+            cText = new char[text.Length];
+            cTextAfter = new char[text.Length];
+           
+
+            text = text.ToLower();
+
+            try
+            {
+                iKey = int.Parse(txtKey.Text);
+            }
+            catch (Exception)
+            {
+
+                txtKey.Text = "Put a correct key";
+
+            }
+
+            iGap = iKey;
+
+            //Test on the key
+
+            if (iKey >= 1 && iKey <= 26)
+            {
+                //all the process
+                bCorrect = true;
+
+                foreach (char c in text)
+                {
+                    cText[idx] = c;
+                    iValue = cText[idx];
+                    iNewLetter = iValue - iGap;
+
+                    cTextAfter[idx] = (char)iNewLetter;
+
+                    if (iNewLetter <96 && iValue != 32 && iValue <= 122 && iValue >64)
+                    {
+                        int iReste = 96 - iNewLetter;
+                        int iNouvValeur = 122 - iReste;
+                        cTextAfter[idx] = (char)iNouvValeur;
+                    }
+                    else
+                    {
+                        if (iValue >= 0 && iNewLetter <= 64 + iGap || iValue > 200 || iValue == 32)
+                        {
+                            cTextAfter[idx] = (char)iValue;
+                        }
+                        if (iValue - iGap == 39)
+                        {
+                            cTextAfter[idx] = (char)iValue;
+                        }
+                        if (iValue == 123 || iValue == 125 || iValue == 91 || iValue == 93)
+                        {
+                            cTextAfter[idx] = (char)iValue;
+                        } 
+                    }
+                    txtDecrypted.Text += cTextAfter[idx].ToString();
+                    idx++;
+                    
+                }
+            }
+            else
+            {
+                txtKey.Text = "Put a correct Key";
+            }
+            //affichage
+
+            
+            
+                
+                    
+                
+                              
+        }
+
+        private void btnOpF_Click(object sender, RoutedEventArgs e)
+        {
+            
+            
+
+            OpenFileDialog ofd; //déclaration du choix de sélection
+            ofd = new OpenFileDialog(); //instanciation de celui ci                          
+            ofd.InitialDirectory = Directory.GetCurrentDirectory(); //On indique dans quel répertoire il doit démarrer
+            ofd.DefaultExt = "txt";
+            // Filtre sur les fichiers
+            ofd.Filter = "fichiers textes (*.txt)|*.txt|Tous les fichiers (*.*)|*.*";
+            ofd.FilterIndex = 1;
+            ofd.ShowDialog();//On l'affiche ensuite à l'écran
+            
+
+
+            if (ofd.FileName != "")
+            {
+                FileInfo fFichierSrce = new FileInfo(ofd.FileName); //On déclare un fichier FileInfo qui stockera les informations du répertoire    
+                StreamReader strmr = new StreamReader(ofd.FileName, Encoding.Default); //On ouvre un flux de lecture dans le répertoire de la boite de sélection
+
+                //On lis les lignes et on les stockes dans les variables
+
+                sLigne = "";
+                sLignes = "";
+                do
+                {
+                    sLigne = strmr.ReadLine();
+                    if (sLigne != null)
+                    {
+                        sLignes += sLigne + "\n";
+                        iNbrLignes += 1;
+                    }
+                } while (sLigne != null);
+
+                //fermeture du flux
+                strmr.Close();
+
+                //On affiche les résultats
+                lblFileName.Content += ofd.FileName.ToString();
+                txtDecrypt.Text = sLignes;
+            }
+            VerifyKey();
+        }
+
+
+
+
+        private void VerifyKey()
+        {
+          char cCaracKey = char.Parse(sLignes.Substring(sLignes.Length - 2, 1));
+          iKey = cCaracKey - 96;
+          txtKey.Text = iKey.ToString();
+        }
+
+
+
+    }
+}
